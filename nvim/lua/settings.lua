1010f6f7fd33c2fd968cff6c1b1d1987462a0a31
mmppppss <ppozosoliz@gmail.com>
2024-03-14 08:29:07 -0400

    add more setup

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -4,7 +4,7 @@ require("plugins.coc-snip")
 require("plugins.lualine")
 vim.wo.number = true
 vim.wo.relativenumber = true
-vim.cmd([[set clipboard=unnamedplus]])
+--vim.cmd([[set clipboard=unnamedplus]])
 vim.o.confirm = true
 vim.o.autoindent = true
 vim.o.tabstop = 4
@@ -15,6 +15,7 @@ vim.opt.backup = false
 vim.opt.writebackup = false
 vim.opt.updatetime = 300
 vim.opt.signcolumn = "yes"
+vim.o.clipboard="unnamedplus"
 vim.cmd([[colorscheme gruvbox]])
 vim.cmd([[set termguicolors]])
 vim.cmd([[
