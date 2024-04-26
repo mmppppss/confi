13d359e3949be5029fac7bafac9ec3fdb102d28e
mmppppss <ppozosoliz@gmail.com>
2024-04-26 06:32:21 -0400

    add ibl (indent blank line) and gruvbox config in plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,28 +1,28 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-    {
+	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.4',
 		dependencies = { 'nvim-lua/plenary.nvim' }
-    },
- --   "folke/tokyonight.nvim",
+	},
 	"ellisonleao/gruvbox.nvim",
-    {
+	{
 		"neoclide/coc.nvim", 
 		branch= "release"
 	},
 	{
-		"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+		"nvim-treesitter/nvim-treesitter", 
+		build = ":TSUpdate"
 	},
 	"terrortylor/nvim-comment",
 	{
@@ -35,46 +35,20 @@ require("lazy").setup({
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
 	},
-     'Exafunction/codeium.vim',
+	'Exafunction/codeium.vim',
 	{
 		'windwp/nvim-autopairs',
 		--event = "InsertEnter",
 		opts = {}
-	}
+	},
+	"puremourning/vimspector",
+    {
+        "lukas-reineke/headlines.nvim",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        config = true, -- or `opts = {}`
+    },
+	'mattn/emmet-vim'
 })
-
-require("ibl").setup({
-       debounce = 100,
-       indent = { char = "│"},
-       whitespace = { highlight = {"Whitespace", "NonText", "Function" } },
-       scope = { enabled=true, exclude = { language = { "lua" } } },
-})
-
-require("gruvbox").setup({
-  terminal_colors = true, -- add neovim terminal colors
-  undercurl = true,
-  underline = true,
-  bold = true,
-  italic = {
-    strings = true,
-    emphasis = true,
-    comments = true,
-    operators = false,
-    folds = true,
-  },
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "hard", -- can be "hard", "soft" or empty string
-  palette_overrides = {},
-  overrides = {},
-  dim_inactive = false,
-  transparent_mode = true,
-})
-
 
 --END LAZY
 require("keymap")
--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -2,15 +2,15 @@ require("plugins.coc")
 require("plugins.treesitter")
 require("plugins.coc-snip")
 require("plugins.lualine")
+require("plugins.gruvbox")
+require("plugins.ibl")
 vim.wo.number = true
 vim.wo.relativenumber = true
---vim.cmd([[set clipboard=unnamedplus]])
 vim.o.confirm = true
 vim.o.autoindent = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.wrap = false
---vim.o.signcolumn = "n"
 vim.opt.backup = false
 vim.opt.writebackup = false
 vim.opt.updatetime = 300
