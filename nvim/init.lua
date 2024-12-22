ff4aa62ec7ac5afc7de9d4224c3327e07ac5e05b
mmppppss <ppozosoliz@gmail.com>
2024-12-22 18:14:23 -0400

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
 	},
 	--'morhetz/gruvbox',
 	"ellisonleao/gruvbox.nvim",
+	"preservim/nerdtree",
 	{
 		"neoclide/coc.nvim", 
 		branch= "release"
@@ -30,13 +31,13 @@ require("lazy").setup({
 		"lukas-reineke/indent-blankline.nvim", 
 		main = "ibl", opts = {} 
 	},
-	'honza/vim-snippets',
 	'junegunn/goyo.vim',
 	{
 		'nvim-lualine/lualine.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
 	},
 	'Exafunction/codeium.vim',
+	'github/copilot.vim',
 	{
 		'windwp/nvim-autopairs',
 		--event = "InsertEnter",
@@ -48,9 +49,37 @@ require("lazy").setup({
     	dependencies = "nvim-treesitter/nvim-treesitter",
     	config = true, -- or `opts = {}`
     },
-	'mattn/emmet-vim'
+	'mattn/emmet-vim',
+	{
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		--build = "make install_jsregexp",
+		dependencies = "rafamadriz/friendly-snippets"
+	},
+	'SirVer/ultisnips',
+	'honza/vim-snippets',
+--	{
+--    	"jghauser/auto-pandoc.nvim",
+--    	dependencies = { "nvim-lua/plenary.nvim" },
+--    	ft = "markdown",
+--	}
 })
 
 --END LAZY
 require("keymap")
 require("settings")
+local ls = require("luasnip")
+
+vim.keymap.set({"i"}, "<C-k>", function() ls.expand() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+
+vim.keymap.set({"i", "s"}, "<C-E>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end, {silent = true})
+require("luasnip.loaders.from_vscode").lazy_load()
+
